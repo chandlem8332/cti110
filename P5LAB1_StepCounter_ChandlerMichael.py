7c69022bd8bce7d6a9006f1ef75ebc8efc66f3c5
chandlem8332 <107379753+chandlem8332@users.noreply.github.com>
2022-07-18 01:28:56 -0400

    Add files via upload
    P5LAB1 Step Counter

---
+++ P5LAB1_StepCounter_ChandlerMichael.py
@@ -0,0 +1,10 @@
+# Define your function here 
+def feet_to_steps(user_feet):
+    steps = user_feet / 2.5
+    return int(steps)
+
+if __name__ == '__main__':
+    # Type your code here.
+    input_feet = float(input())
+    steps = feet_to_steps(input_feet)
+    print(steps)
